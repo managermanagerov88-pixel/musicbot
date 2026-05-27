f3f2f0291ae2fafa584f054091c1a62766a09225
managermanagerov88-pixel <managermanagerov88@gmail.com>
2026-05-28 00:49:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-BOT_TOKEN = "ВСТАВЬ_ТОКЕН_ОТ_BOTFATHER"
+BOT_TOKEN = "8994533338:AAEouqVsEXkiRLViw2I1RucsEkKswUZP5RY"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("🎵 Бот работает! Отправь аудио или голосовое.")
+    await message.answer("🎵 Бот работает! Отправь мне голосовое или аудио.")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
